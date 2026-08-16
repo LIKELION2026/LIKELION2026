222cc8d5acfd603361bc9dea5f919d97f78d8365
hoon-ground <break_the_limit@naver.com>
2026-08-16 17:05:48 +0900

    feat(livekit): webhook 서명 검증 수신기 추가

--- apps/server/src/integrations/livekit/livekit.module.ts
+++ apps/server/src/integrations/livekit/livekit.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 
 import { LiveKitTokenService } from "./livekit-token.service";
+import { LiveKitWebhookService } from "./livekit-webhook.service";
 
 @Module({
-  exports: [LiveKitTokenService],
-  providers: [LiveKitTokenService]
+  exports: [LiveKitTokenService, LiveKitWebhookService],
+  providers: [LiveKitTokenService, LiveKitWebhookService]
 })
 export class LiveKitModule {}
